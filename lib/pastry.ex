18bbc73d7205d452567f3490f4df1bd5eae0d518
Kumar Rohit Malhotra <krohitm@gmail.com>
2017-10-20 13:59:29 -0400

    checking GenServers

--- lib/pastry.ex
+++ lib/pastry.ex
@@ -1,18 +1,79 @@
 defmodule Pastry do
-  @moduledoc """
-  Documentation for Pastry.
-  """
+use GenServer
+  #spawning actors
+  def pastryInit(_, 0) do
+    true
+  end
+  def pastryInit(numNodes) do
+    newNode = Pastry.spawnProcess(numNodes)
+    newNode |> pastryInit(numNodes-1)
+  end
+  def pastryInit(nearbyNode, numNodes) do
+    newNode = Pastry.spawnProcess(numNodes)
+    Pastry.sendJoin(newNode, nearbyNode)
+    newNode |> pastryInit(numNodes-1)
+  end
 
-  @doc """
-  Hello world.
+  def spawnProcess(nodeCounter) do
+    nodeId = :md5
+            |> :crypto.hash(to_string(nodeCounter)) 
+            |> Base.encode16() 
+            |> String.to_atom
+    {:ok, pid} = GenServer.start(__MODULE__, :createdNode, name: nodeId)
+    #nodeId = :md5 |> :crypto.hash(:erlang.pid_to_list(pid)) |> Base.encode16() 
+    :global.register_name(nodeId, pid)
+    newNode = nodeId
+  end
 
-  ## Examples
+  def init(state) do
+    {:ok, state}
+  end
 
-      iex> Pastry.hello
-      :world
+  #new node sends join to nearest node
+  def sendJoin(newNode, nearbyNode) do
+    #IO.inspect :global.registered_names
+    GenServer.cast(newNode, {:joinNetwork, nearbyNode, newNode})
+  end
 
-  """
-  def hello do
-    :world
+  #new node sends :join message to "assumed" nearby node
+  def handle_cast({:joinNetwork, nearbyNode, newNode}, currentState) do
+    GenServer.cast(nearbyNode, {:join, newNode})
+    { :noreply, :joinedNetwork}
+  end
+  #nearby node selected by the new node receives the join message
+  def handle_cast({:join, key}, curState) do
+    IO.puts key
+    { :noreply, curState}
+  end
+
+  
+  def main(args) do
+
+    numNodes = args 
+              |> parse_args 
+              |> Enum.at(0)
+              |> Integer.parse(10) 
+              |> elem(0) 
+
+    numRequests = args 
+                  |> parse_args 
+                  |> Enum.at(1)
+                  |> Integer.parse(10) 
+                  |> elem(0) 
+
+    #{:ok, serverPid} = GenServer.start(__MODULE__, server, name: :server)
+    #GenServer.call(serverPid, )
+    Pastry.pastryInit(numNodes)
+    receive do
+      :over ->
+        true
+    end
+  end
+
+  #parsing the input argument
+  defp parse_args(args) do
+    {_, word, _} = args 
+    |> OptionParser.parse(strict: [:integer, :integer])
+    word
   end
 end
--- mix.exs
+++ mix.exs
@@ -3,10 +3,13 @@ defmodule Pastry.Mixfile do
 
   def project do
     [
-      app: :pastry,
+      app: :project3,
       version: "0.1.0",
-      elixir: "~> 1.5",
+      #elixir: "~> 1.5",
       start_permanent: Mix.env == :prod,
+      escript: [main_module: Pastry,
+                emu_args: [ "+P 5000000" ]
+                ],
       deps: deps()
     ]
   end
